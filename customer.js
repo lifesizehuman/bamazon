7a27dc7ff8a553b1c95bc8f82e848eff951e01ed
Slater Combes <combes.slater@gmail.com>
2017-09-10 10:28:20 -0700

    first complete of customer js

--- customer.js
+++ customer.js
@@ -1,25 +1,19 @@
 var mysql = require('mysql')
 var inquirer = require('inquirer')
 
-var connection = mysql.createConnection({
-  host: 'localhost',
-  port: 8889,
-  user: 'root',
-  password: 'insecure',
-  database: 'bamazon'
-})
+var connection = mysql.createConnection({host: 'localhost', port: 8889, user: 'root', password: 'insecure', database: 'bamazon'})
 
-connection.connect(function (err) {
+connection.connect(function(err) {
   if (err) {
     return console.log(err)
   }
   queryBamazon()
 })
 
-function queryBamazon () {
+function queryBamazon() {
   console.log("Items up for sale")
   console.log('------------------')
-  connection.query('SELECT * FROM products', function (err, res) {
+  connection.query('SELECT * FROM products', function(err, res) {
     for (var i = 0; i < res.length; i++) {
       console.log(res[i].item_id + ' | ' + res[i].product_name + ' | ' + "$" + res[i].price + ' | ' + res[i].quantity);
       console.log('------------------');
@@ -33,16 +27,18 @@ var mainMenu = function() {
     {
       type: "list",
       message: "Please select:",
-      choices: ["Exit", "Buy an Item"],
+      choices: [
+        "Exit", "Buy an Item"
+      ],
       name: "choice"
     }
-  ]).then(function(res){
-    switch(res.choice){
+  ]).then(function(res) {
+    switch (res.choice) {
       case("Exit"):
         connection.end();
         return;
         break;
-      case ("Buy an Item"):
+      case("Buy an Item"):
         buyItem();
         break;
     }
@@ -51,5 +47,28 @@ var mainMenu = function() {
 
 var buyItem = function() {
   console.log("This is still in production.");
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "enter the name of the item you would like to buy",
+      name: "product"
+    }, {
+      type: "number",
+      message: "how many do you want to buy?",
+      name: "quantity"
+    }
+  ]).then(function(argument) {
+    connection.query("SELECT quantity FROM products WHERE product_name = ?", [argument.product], function(err, res) {
+      if (err) {
+        return console.log(err);
+      }
+      if (res[0].quantity < argument.quantity) {
+        return console.log("ERROR: Insufficient store quantity.");
+      }
+      connection.query("UPDATE products SET quantity WHERE product_name = ?", [argument.product]);
+      console.log("Your order has been placed.");
+      queryBamazon();
+    });
+  });
   mainMenu();
 }
